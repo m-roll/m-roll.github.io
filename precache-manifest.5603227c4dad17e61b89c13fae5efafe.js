0d960dc1c2d6d6e1031409f3af95639ad28637bb
Matt Rollender <mroll@mit.edu>
2019-04-19 11:30:44 -0400

    all statics

---
+++ precache-manifest.5603227c4dad17e61b89c13fae5efafe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71b50c7b9c268f477a13",
+    "url": "/static/css/main.c7831d4e.chunk.css"
+  },
+  {
+    "revision": "71b50c7b9c268f477a13",
+    "url": "/static/js/main.7fbc50f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aca6930d73183802acb5",
+    "url": "/static/css/2.f81a63d2.chunk.css"
+  },
+  {
+    "revision": "aca6930d73183802acb5",
+    "url": "/static/js/2.5842b411.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8653b20b7629df9d99fa74d5b5c97c08",
+    "url": "/index.html"
+  }
+];
